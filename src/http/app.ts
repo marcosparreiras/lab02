4bb8f7232ebef72be5b88ae292bb7d66ed8123f8
Marcos Parreiras <marcosalp1998@gmail.com>
2024-07-07 21:49:06 -0300

    fix: postgresSQL injection on http server

--- src/http/app.ts
+++ src/http/app.ts
@@ -4,8 +4,10 @@ import { SqlUserRepository } from "../adapters/sql-user-repository";
 import { createUserController } from "./routes/create-user-controller";
 import { getUserController } from "./routes/get-user-controller";
 import { errorHandler } from "./middlewares/error-handles";
+import { PostgresSQL } from "../adapters/postgres-sql";
 
-const userRepository = new SqlUserRepository();
+const postgresSQL = new PostgresSQL();
+const userRepository = new SqlUserRepository(postgresSQL);
 Registry.getInstance().setUserRepository(userRepository);
 
 export const app = fastify();
